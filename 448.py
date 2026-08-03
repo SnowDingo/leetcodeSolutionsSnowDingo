2865246eee7109d0bc1764f19d2f9dd5563bd11c
snowdingo <snowdingo.tech@gmail.com>
2026-08-03 11:37:37 +0900

    solved problem 448 with a hashSet

---
+++ 448.py
@@ -0,0 +1,10 @@
+# A solution that utilizes the property of hashSet
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        num = set(nums)
+        result = []
+        for i in range(1,len(nums)+1):
+            if(i not in num):
+                result.append(i)
+        return result
